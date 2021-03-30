5ee9eb341d9adac29f5a423cbea0bd4b0c598fa2
gitgeoman <gitgeoman@gmail.com>
2021-03-30 13:51:31 +0200

    Initialize project using Create React App

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyD00MW6yw4DpfNNw63pAKcLMn3uBkN-SVg",
+  authDomain: "clone-d1b7c.firebaseapp.com",
+  projectId: "clone-d1b7c",
+  storageBucket: "clone-d1b7c.appspot.com",
+  messagingSenderId: "94939419002",
+  appId: "1:94939419002:web:10f8eb67a40ca6a64d46c3",
+  measurementId: "G-EFS94VB085",
+};
