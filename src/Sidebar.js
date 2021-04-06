10199b1e7f23af711bd539f5ca00f90e56774293
gitgeoman <gitgeoman@gmail.com>
2021-04-06 15:18:49 +0200

    Update Sidebar.js

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -2,7 +2,16 @@ import React from "react";
 import "./Sidebar.css";
 
 function Sidebar() {
-  return <div className="sidebar">im a sidebar</div>;
+  return (
+    <div className="sidebar">
+      im a sidebar
+      <SidebarRow title="home" />
+      <SidebarRow title="trending" />
+      <SidebarRow title="Subscription" />
+      <SidebarRow title="" />
+      <SidebarRow title="" />
+    </div>
+  );
 }
 
 export default Sidebar;
