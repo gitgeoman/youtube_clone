1928e697ef991ec3b1977694837ead3c9c1cf260
gitgeoman <gitgeoman@gmail.com>
2021-04-07 09:20:24 +0200

    Sidebar update

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,15 +1,36 @@
 import React from "react";
+
+//internal components import
 import "./Sidebar.css";
+import SidebarRow from "./SidebarRow";
+
+//icons from material@ui
+import HomeSharpIcon from "@material-ui/icons/HomeSharp";
+import WhatshotSharpIcon from "@material-ui/icons/WhatshotSharp";
+import SubscriptionsSharpIcon from "@material-ui/icons/SubscriptionsSharp";
+
+import VideoLibrarySharpIcon from "@material-ui/icons/VideoLibrarySharp";
+import HistorySharpIcon from "@material-ui/icons/HistorySharp";
+import OndemandVideoSharpIcon from "@material-ui/icons/OndemandVideoSharp";
+import WatchLaterSharpIcon from "@material-ui/icons/WatchLaterSharp";
+import ThumbUpAltSharpIcon from "@material-ui/icons/ThumbUpAltSharp";
+import ExpandMoreSharpIcon from "@material-ui/icons/ExpandMoreSharp";
 
 function Sidebar() {
   return (
     <div className="sidebar">
-      im a sidebar
-      <SidebarRow title="home" />
-      <SidebarRow title="trending" />
-      <SidebarRow title="Subscription" />
-      <SidebarRow title="" />
-      <SidebarRow title="" />
+      <SidebarRow selected Icon={HomeSharpIcon} title="Home" />
+      <SidebarRow Icon={WhatshotSharpIcon} title="Trending" />
+      <SidebarRow Icon={SubscriptionsSharpIcon} title="Subscription" />
+      <hr />
+      <SidebarRow Icon={VideoLibrarySharpIcon} title="Subscription" />
+      <SidebarRow Icon={HistorySharpIcon} title="Subscription" />
+      <SidebarRow Icon={OndemandVideoSharpIcon} title="Subscription" />
+      <SidebarRow Icon={WatchLaterSharpIcon} title="Subscription" />
+      <SidebarRow Icon={ThumbUpAltSharpIcon} title="Subscription" />
+      <SidebarRow Icon={SubscriptionsSharpIcon} title="Subscription" />
+      <SidebarRow Icon={ExpandMoreSharpIcon} title="Subscription" />
+      <hr />
     </div>
   );
 }
